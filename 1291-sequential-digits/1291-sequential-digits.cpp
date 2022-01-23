2248c6e43dcd5e0966b2e76cd59fd2ff617c354b
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-01-23 17:16:28 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.79%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         
         vector<int> ans;
-        int k=2,st=1;
+        /*int k=2,st=1;
         for(int i=1;i<9;i++)
         {
             st=i;
@@ -14,7 +14,7 @@ public:
                     ans.push_back(st);
             }
         }
-        sort(ans.begin(),ans.end());
+        sort(ans.begin(),ans.end());*/
         /*for(auto itr = ans.begin();itr!=ans.end();itr++)
         {
             //cout<<*itr<<" ";
@@ -27,8 +27,23 @@ public:
             }
                 
         }*/
+        // Using BFS   
+        queue<int> q{{1,2,3,4,5,6,7,8}};
+        while(!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+            if(cur > high)
+                break;
+            if(cur >= low and cur <=high)
+                ans.push_back(cur);
             
-        
+            int rem = cur%10;
+            int next = cur*10 + rem + 1;
+            if(rem < 9)
+                q.push(next);
+            
+        }
         return ans;
     }
 };

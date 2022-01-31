2c44d45fcbe26bc0aaf6741f51c4f8e923a445eb
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-01-31 19:10:23 +0530

    Time: 73 ms (68.76%), Space: 14.4 MB (93.02%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,38 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        def arrange_board(board):
+            new_board = []
+            for row in board:
+                new_board.append("".join(row))
+            return new_board
+        
+        def solve(row, cols, diagonals, anti_diagonals, board):
+            if row == n:
+                ans.append(arrange_board(board))
+                return
+            
+            for col in range(n):
+                curr_diag = row - col
+                curr_anti_diag = row + col
+                
+                if col in cols or curr_diag in diagonals or curr_anti_diag in anti_diagonals:
+                    continue
+                    
+                cols.add(col)
+                diagonals.add(curr_diag)
+                anti_diagonals.add(curr_anti_diag)
+                board[row][col] = "Q"
+                
+                solve(row + 1, cols, diagonals, anti_diagonals, board)
+                
+                cols.remove(col)
+                diagonals.remove(curr_diag)
+                anti_diagonals.remove(curr_anti_diag)
+                board[row][col] = "."
+        
+        ans = []
+        initial_board = [["."]*n for i in range(n)]
+        solve(0, set(), set(), set(), initial_board)
+        
+        return ans

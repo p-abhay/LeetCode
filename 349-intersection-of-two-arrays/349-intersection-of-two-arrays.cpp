3878de338f3abadd09a77f0f9a65ec12c52839f5
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-02-21 13:34:16 +0530

    Time: 8 ms (60.62%), Space: 10.8 MB (15.27%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> num1;
+        map<int,int> num2;
+        for(auto i : nums1)
+            num1[i]++;
+        for(auto i : nums2)
+            num2[i]++;
+        vector<int> ans;
+        //if(num1.size() < num2.size())
+        /*{*/
+            for(auto itr = num1.begin(); itr != num1.end(); itr++)
+            {
+                if(num2.find(itr->first) != num2.end())
+                {
+                    ans.push_back(itr->first);
+                        
+                }
+            }
+        /*}*/
+        return ans;
+    }
+};

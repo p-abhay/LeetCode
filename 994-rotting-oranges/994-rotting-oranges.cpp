05c1f7a32c93308e9a394861c83f7be2fe6676cd
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-03-11 19:25:44 +0530

    Time: 8 ms (72.35%), Space: 13.2 MB (33.62%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -14,47 +14,47 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        queue<Node*> q;
+        queue<Node> q;
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j  < cols; j++) {
                 if(grid[i][j] == 2) {
-                    Node* node = new Node(i, j, 0);
+                    Node node = Node(i, j, 0);
                     q.push(node);
                 }
             }
         }
         int ans = 0;
         while(!q.empty()) {
-            Node* currNode = q.front();
+            Node currNode = q.front();
             q.pop();
-            int x = currNode->x;
-            int y = currNode->y;
-            int time = currNode->time;
+            int x = currNode.x;
+            int y = currNode.y;
+            int time = currNode.time;
             
             if(x - 1 >= 0 && grid[x - 1][y] == 1) {
                 grid[x - 1][y] = 2;
-                Node* node = new Node(x - 1, y, time + 1);
+                Node node = Node(x - 1, y, time + 1);
                 q.push(node);
                 ans = time + 1;
             }
             
             if(x + 1 < rows && grid[x + 1][y] == 1) {
                 grid[x + 1][y] = 2;
-                Node* node = new Node(x + 1, y, time + 1);
+                Node node = Node(x + 1, y, time + 1);
                 q.push(node);
                 ans = time + 1;
             }
             
             if(y - 1 >= 0 && grid[x][y - 1] == 1) {
                 grid[x][y - 1] = 2;
-                Node* node = new Node(x, y - 1, time + 1);
+                Node node = Node(x, y - 1, time + 1);
                 q.push(node);
                 ans = time + 1;
             }
             
             if(y + 1 < cols && grid[x][y + 1] == 1) {
                 grid[x][y + 1] = 2;
-                Node* node = new Node(x, y + 1, time + 1);
+                Node node = Node(x, y + 1, time + 1);
                 q.push(node);
                 ans = time + 1;
             }

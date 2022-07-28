ab19ff024377f5eec234e4befaaf04e230094d42
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-07-28 20:28:05 +0530

    Time: 24 ms (19.33%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mp1,mp2;
-        for(auto i : s)
-            mp1[i] += 1;
-        for(auto i : t)
-            mp2[i] += 1;
-        auto itr1 = mp1.begin(), itr2 = mp2.begin();
-        for(;itr1 != mp1.end() && itr2 != mp2.end();itr1++,itr2++) {
-            if(itr1->first == itr2->first && itr2->second == itr1->second)
-                continue;
-            return false;
+        unordered_map<char,int> mp;
+        int i = 0, j = 0;
+        while((i < s.size()) || (j < t.size()) ) {
+            mp[s[i++]] += 1;
+            mp[t[j++]] -= 1;
         }
-        if(itr1 != mp1.end() || itr2 != mp2.end())
-            return false;
+        for(auto i : mp) {
+            cout<<i.second<<"\n";
+            if(i.second)
+                return false;
+        }
         return true;
     }
 };

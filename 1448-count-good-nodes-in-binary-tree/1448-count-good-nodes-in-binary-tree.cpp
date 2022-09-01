1b4bcfc7f1b20a6be57eb933c57bad0792776748
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-09-01 21:59:32 +0530

    Time: 257 ms (16.74%), Space: 86.3 MB (54.67%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -14,24 +14,17 @@ public:
     int goodNodes(TreeNode* root) {
         int max = root->val;
         int ans = 0;
-        solve(root,max,ans);
+        dfs(root,max,ans);
         return ans;
     }
-    void solve(TreeNode* root, int& max, int& ans) {
-        set<TreeNode*> visited;
-        dfs(root,max,ans,visited);
-    }
-    void dfs(TreeNode* root, int max, int& ans, set<TreeNode*>& visited) {
+    void dfs(TreeNode* root, int max, int& ans) {
         if(root == nullptr)
             return;
         if(root->val >= max) {
             max = root->val;
             ans += 1;
         }
-        visited.insert(root);
-        if(visited.find(root->left) == visited.end())
-            dfs(root->left, max, ans,visited);
-        if(visited.find(root->right) == visited.end())
-            dfs(root->right, max, ans,visited);
+        dfs(root->left, max, ans);
+        dfs(root->right, max, ans);
     }
 };

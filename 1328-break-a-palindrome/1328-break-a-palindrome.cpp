6263925a1cd5b38aefb4aefceb1c1382ef52dbc4
Abhay Sankhwar <97156344+p-abhay@users.noreply.github.com>
2022-10-10 22:34:50 +0530

    Time: 8 ms (10.44%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool check(string s) {
+        int l = 0,r = s.size()-1;
+        //cout<<s<<"in";
+        while(l < r) {
+            if(s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    string breakPalindrome(string s) {
+        int n = s.size();
+        if(n == 1)
+            return "";
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'a') {
+                cnt += 1;
+                continue;
+            }
+            char ch = s[i];
+            s[i] = 'a';
+            
+            if(!check(s)) {
+                cout<<s<<" ";
+                return s;
+            }
+                
+            s[i] = ch;
+        }
+        //if(cnt == n)
+            s[n-1] = 'b';
+        return s;
+    }
+};
